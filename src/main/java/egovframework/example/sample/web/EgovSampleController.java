854c5f86223b5644d2410aa2abb589b7db44092a
mjw <jwmin@goodmit.co.kr>
2024-02-06 16:57:22 +0900

    push

--- src/main/java/egovframework/example/sample/web/EgovSampleController.java
+++ src/main/java/egovframework/example/sample/web/EgovSampleController.java
@@ -30,10 +30,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.ui.ModelMap;
 import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.bind.support.SessionStatus;
 import org.springmodules.validation.commons.DefaultBeanValidator;
 
@@ -76,6 +73,12 @@ public class EgovSampleController {
 	 * @return "egovSampleList"
 	 * @exception Exception
 	 */
+
+	@GetMapping(value="/")
+	public String get(){
+		return "hellow";
+	}
+
 	@RequestMapping(value = "/egovSampleList.do")
 	public String selectSampleList(@ModelAttribute("searchVO") SampleDefaultVO searchVO, ModelMap model) throws Exception {
 
